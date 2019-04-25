27ffb1990a62191ce10a89a8b10cd55b679e875e
helixd.2k18 <helixd.2k18@gmail.com>
2019-04-25 18:35:24 +0800

    delay Rust version

--- rtvg/src/lib.rs
+++ rtvg/src/lib.rs
@@ -1,5 +1,15 @@
 #[macro_use]
 extern crate ash;
+#[cfg(target_os = "windows")]
+extern crate winapi;
+
+#[cfg(target_os = "macos")]
+extern crate cocoa;
+#[cfg(target_os = "macos")]
+extern crate metal_rs as metal;
+#[cfg(target_os = "macos")]
+extern crate objc;
+extern crate winit;
 
 #[macro_use]
 pub mod modules;
